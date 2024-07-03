02dd9d1b3f231fab851ae2ee0dde255f88e7d428
Mohammed-salah12 <mohammedsalah899000@gmail.com>
2024-07-03 14:05:48 +0300

    3D-iphone-THREE-JS

--- src/componnents/Model.jsx
+++ src/componnents/Model.jsx
@@ -1,6 +1,6 @@
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
-import ModelView from "./ModelView";
+import ModelView from "./ModelView.jsx";
 import { useRef, useState } from "react";
 import { yellowImg } from '../utils';
 import * as THREE from 'three';
@@ -44,11 +44,28 @@ const Model = () => {
                 <h1 id="heading" className="section-heading">
                     Take a closer look.
                 </h1>
-                <div className="flex flex-col mt-5 items-center">
-                    <div className="w-full h-(75vh) md:h-[90vh] overflow-hidden relative">
-                        <ModelView index={1} groupRef={small} gsapType='view1' controlRef={cameraControlSmall} setRotationState={setSmallRotation} item={model} size={size} />
-                        <ModelView index={2} groupRef={large} gsapType='view2' controlRef={cameraControlLarge} setRotationState={setLargeRotation} item={model} size={size} />
 
+                <div className="flex flex-col items-center mt-5">
+                    <div className="w-full h-[75vh] md:h-[90vh] overflow-hidden relative">
+                        <ModelView
+                            index={1}
+                            groupRef={small}
+                            gsapType="view1"
+                            controlRef={cameraControlSmall}
+                            setRotationState={setSmallRotation}
+                            item={model}
+                            size={size}
+                        />
+
+                        <ModelView
+                            index={2}
+                            groupRef={large}
+                            gsapType="view2"
+                            controlRef={cameraControlLarge}
+                            setRotationState={setLargeRotation}
+                            item={model}
+                            size={size}
+                        />
 
                         <Canvas
                             className="w-full h-full"
@@ -65,10 +82,10 @@ const Model = () => {
                             <View.Port />
                         </Canvas>
                     </div>
+
                     <div className="mx-auto w-full">
-                        <p className="text-sm font-light text-center mb-5 ">
-                            {model.title}
-                        </p>
+                        <p className="text-sm font-light text-center mb-5">{model.title}</p>
+
                         <div className="flex-center">
                             <ul className="color-container">
                                 {models.map((item, i) => (
@@ -85,7 +102,6 @@ const Model = () => {
                             </button>
                         </div>
                     </div>
-
                 </div>
             </div>
         </section>
